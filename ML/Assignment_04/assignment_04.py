391039167932c92dd4bbdbb2385a5711f6636ad1
harihp77 <harihp@gmail.com>
2016-05-31 09:23:31 -0700

    Build matrix, add 1st column with 1

--- ML/Assignment_04/assignment_04.py
+++ ML/Assignment_04/assignment_04.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 
 df = pd.read_excel("a4.xlsx")
+df.insert(0,"DummyWeight", 1)
 table = df.as_matrix()
 print "Rows and columes", np.shape(table)
 print table
